355e3b6d891b6d4ed193c7d27b0524de1c3c4183
Kalle Launiala <kalle.launiala@protonit.net>
2014-11-23 18:47:26 +0200

    Fixed reference to environment variable

--- TCUpdateWorkers.cmd
+++ TCUpdateWorkers.cmd
@@ -2,7 +2,7 @@
 CD "%~dp0"
 
 SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%TBSTORAGEACCOUNTNAME%;AccountKey=%TBSTORAGEACCOUNTKEY%
-SET DestUri=https://%1.blob.core.windows.net/
+SET DestUri=https://%TBSTORAGEACCOUNTNAME%.blob.core.windows.net/
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\*.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\CaloomWorkerRole.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 
