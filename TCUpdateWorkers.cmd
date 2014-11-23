76f440350a06c83f4c3070c7d3f3dc2d6d888a5f
Kalle Launiala <kalle.launiala@protonit.net>
2014-11-23 18:42:02 +0200

    TeamCity script for updating workers

---
+++ TCUpdateWorkers.cmd
@@ -0,0 +1,8 @@
+%~d0
+CD "%~dp0"
+
+SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%TBSTORAGEACCOUNTNAME%;AccountKey=%TBSTORAGEACCOUNTKEY%
+SET DestUri=https://%1.blob.core.windows.net/
+Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\*.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
+Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\CaloomWorkerRole.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
+
