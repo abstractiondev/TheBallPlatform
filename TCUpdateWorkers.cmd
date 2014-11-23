445cb4debe60ef23eedf01903ef2cee6d6a431cd
Kalle Launiala <kalle.launiala@protonit.net>
2014-11-23 19:08:58 +0200

    Added exit 0 to script for signaling OK result

--- TCUpdateWorkers.cmd
+++ TCUpdateWorkers.cmd
@@ -6,3 +6,4 @@ SET DestUri=https://%TBSTORAGEACCOUNTNAME%.blob.core.windows.net/
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\*.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\CaloomWorkerRole.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 
+exit 0
