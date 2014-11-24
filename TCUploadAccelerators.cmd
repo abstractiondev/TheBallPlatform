ac686ebf4ae73611ff1956184e412fbe772ba531
Kalle Launiala <kalle.launiala@protonit.net>
2014-11-24 06:34:20 +0200

    Renamed accelerator infra container name to start with alphabetically first

--- TCUploadAccelerators.cmd
+++ TCUploadAccelerators.cmd
@@ -3,7 +3,7 @@ CD "%~dp0"
 
 SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%TBStorageAccountName%;AccountKey=%TBStorageAccountKey%
 SET DestUri=https://%TBStorageAccountName%.blob.core.windows.net/
-Tools\Ext\CloudCopy.exe "%~dp0Apps\AzureConfiguration\bin\Release\app.publish\AzureConfiguration.cspkg" "%DestUri%theball-infra-package" "%DestStorage%"
-Tools\Ext\CloudCopy.exe "C:\SecureParts\ServiceConfiguration.%TBAzureConfigName%.cscfg" "%DestUri%theball-infra-package" "%DestStorage%"
+Tools\Ext\CloudCopy.exe "%~dp0Apps\AzureConfiguration\bin\Release\app.publish\AzureConfiguration.cspkg" "%DestUri%aaa-theball-infra-package" "%DestStorage%"
+Tools\Ext\CloudCopy.exe "C:\SecureParts\ServiceConfiguration.%TBAzureConfigName%.cscfg" "%DestUri%aaa-theball-infra-package" "%DestStorage%"
 
 exit 0
