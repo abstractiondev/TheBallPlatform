2724f4c4ad83684736e341142c49227511cbaca7
Kalle Launiala <kalle.launiala@protonit.net>
2014-11-26 23:12:49 +0200

    Fixed naming convention of Azure related storage and config stuff in scripts

--- TCUpdateWorkers.cmd
+++ TCUpdateWorkers.cmd
@@ -1,8 +1,8 @@
 %~d0
 CD "%~dp0"
 
-SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%TBSTORAGEACCOUNTNAME%;AccountKey=%TBSTORAGEACCOUNTKEY%
-SET DestUri=https://%TBSTORAGEACCOUNTNAME%.blob.core.windows.net/
+SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%StorageAccountName%;AccountKey=%StorageAccountKey%
+SET DestUri=https://%StorageAccountName%.blob.core.windows.net/
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\*.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 Tools\Ext\CloudCopy.exe "%~dp0bin\CaloomWorkerRole\CaloomWorkerRole.dll" "%DestUri%worker-role-accelerator" "%DestStorage%"
 
--- TCUploadAccelerators.cmd
+++ TCUploadAccelerators.cmd
@@ -1,9 +1,9 @@
 %~d0
 CD "%~dp0"
 
-SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%TBStorageAccountName%;AccountKey=%TBStorageAccountKey%
-SET DestUri=https://%TBStorageAccountName%.blob.core.windows.net/
+SET DestStorage=DefaultEndpointsProtocol=https;AccountName=%StorageAccountName%;AccountKey=%StorageAccountKey%
+SET DestUri=https://%StorageAccountName%.blob.core.windows.net/
 Tools\Ext\CloudCopy.exe "%~dp0Apps\AzureConfiguration\bin\Release\app.publish\AzureConfiguration.cspkg" "%DestUri%aaa-theball-infra-package" "%DestStorage%"
-Tools\Ext\CloudCopy.exe "C:\SecureParts\ServiceConfiguration.%TBAzureConfigName%.cscfg" "%DestUri%aaa-theball-infra-package" "%DestStorage%"
+Tools\Ext\CloudCopy.exe "C:\SecureParts\%AzureConfigurationName%.cscfg" "%DestUri%aaa-theball-infra-package" "%DestStorage%"
 
 exit 0
