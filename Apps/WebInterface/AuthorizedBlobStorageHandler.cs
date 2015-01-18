00b144ad18f4044fee911715d36e933994b9f198
Kalle Launiala <kalle.launiala@protonit.net>
2015-01-19 00:35:08 +0200

    Fixed backslash escaping in net use

--- Apps/WebInterface/AuthorizedBlobStorageHandler.cs
+++ Apps/WebInterface/AuthorizedBlobStorageHandler.cs
@@ -499,7 +499,7 @@ namespace WebInterface
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 startInfo.FileName = "cmd.exe";
-                startInfo.Arguments = String.Format("/C net use X: \\{0}.file.core.windows.net\tbcore /u:{0} {1}",
+                startInfo.Arguments = String.Format("/C net use X: \\\\{0}.file.core.windows.net\\tbcore /u:{0} {1}",
                     shareAndUserName, shareKeyName);
 
                 process.StartInfo = startInfo;
