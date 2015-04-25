a9e9c8d7b9e8ddde539075d6595755b3b1b68a03
Kalle Launiala <kalle.launiala@protonit.net>
2015-04-25 16:03:25 +0300

    Initial implementation for Protobuf HttpRequestPackage serialization and deserialization

---
+++ Apps/AzureSupport/HttpRequestSerializer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using ProtoBuf;
+
+namespace AzureSupport
+{
+    public static class HttpRequestSerializer
+    {
+        public class SyncingStream : Stream
+        {
+            private byte[] internalBuffer;
+            private bool ended;
+            private static ManualResetEvent dataAvailable = new ManualResetEvent(false);
+            private static ManualResetEvent dataEmpty = new ManualResetEvent(true);
+
+            public override bool CanRead
+            {
+                get { return true; }
+            }
+
+            public override bool CanSeek
+            {
+                get { return false; }
+            }
+
+            public override bool CanWrite
+            {
+                get { return true; }
+            }
+
+            public override void Flush()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override long Length
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public override long Position
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                dataAvailable.WaitOne();
+                if (count >= internalBuffer.Length)
+                {
+                    var retVal = internalBuffer.Length;
+                    Array.Copy(internalBuffer, buffer, retVal);
+                    internalBuffer = null;
+                    dataAvailable.Reset();
+                    dataEmpty.Set();
+                    return retVal;
+                }
+                else
+                {
+                    Array.Copy(internalBuffer, buffer, count);
+                    internalBuffer = internalBuffer.Skip(count).ToArray(); // i know
+                    return count;
+                }
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void SetLength(long value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                dataEmpty.WaitOne();
+                dataEmpty.Reset();
+
+                internalBuffer = new byte[count];
+                Array.Copy(buffer, internalBuffer, count);
+
+                Debug.WriteLine("Writing some data");
+
+                dataAvailable.Set();
+            }
+
+            public void End()
+            {
+                dataEmpty.WaitOne();
+                dataEmpty.Reset();
+
+                internalBuffer = new byte[0];
+
+                Debug.WriteLine("Ending writes");
+
+                dataAvailable.Set();
+            }
+
+        }
+
+        static HttpRequestSerializer()
+        {
+            Serializer.PrepareSerializer<HttpRequestPackage>();
+        }
+
+        public static void ToStream(this HttpRequestPackage package, Stream stream)
+        {
+            Serializer.Serialize(stream, package);
+        }
+
+        public static T DeserializeProtobuf<T>(this Stream stream)
+        {
+            return Serializer.Deserialize<T>(stream);
+        }
+
+        [ProtoContract]
+        public class HttpRequestPackage
+        {
+            [ProtoMember(1)]
+            public string ContentPath;
+
+            [ProtoMember(2)]
+            public Dictionary<string, string> Paramters;
+
+            [ProtoMember(3)]
+            public Dictionary<string, string> FormValues;
+
+            //[ProtoMember(3)] public HttpFileCollection FileCollection;
+            [ProtoMember(4)] public Dictionary<string, byte[]> FileCollection;
+            
+            [ProtoMember(5)]
+            public byte[] RequestContent;
+
+            [ProtoMember(6)] public string AccountId;
+
+            [ProtoMember(7)] public string OwnerRoot;
+
+        }
+    }
+}
---
+++ Tests/PlatformCoreTests/HttpRequestSerializeTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Web;
+using AzureSupport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlatformCoreTests
+{
+    [TestClass]
+    public class HttpRequestSerializeTests
+    {
+        [TestMethod]
+        public void SerializeToStream()
+        {
+            //PrivateType httpFileCollectionType = new PrivateType(typeof(HttpFileCollection));
+            PrivateObject httpFileCollection = new PrivateObject(typeof(HttpFileCollection));
+            var serData = new HttpRequestSerializer.HttpRequestPackage
+            {
+                AccountId = "acctid",
+                ContentPath = "contentpath",
+                //FileCollection = (HttpFileCollection) httpFileCollection.Target,
+                FileCollection = new Dictionary<string, byte[]>()
+                {
+                    {"filetest", new byte[] { 3, 4, 5, 7} }
+                },
+                FormValues = new Dictionary<string, string>()
+                {
+                    {"formval1", "val1"},
+                    {"formval2", "val2"},
+                },
+                OwnerRoot = "ownerroot",
+                RequestContent = new byte[] {1, 2, 3, 4, 7}
+            };
+            var outputStream = new MemoryStream();
+            serData.ToStream(outputStream);
+            var outputData = outputStream.ToArray();
+            Assert.AreEqual(93, outputData.Length);
+        }
+
+        [TestMethod]
+        public void SerializeToDeserialize()
+        {
+            //PrivateType httpFileCollectionType = new PrivateType(typeof(HttpFileCollection));
+            PrivateObject httpFileCollection = new PrivateObject(typeof(HttpFileCollection));
+            var serData = new HttpRequestSerializer.HttpRequestPackage
+            {
+                AccountId = "acctid",
+                ContentPath = "contentpath",
+                //FileCollection = (HttpFileCollection) httpFileCollection.Target,
+                FileCollection = new Dictionary<string, byte[]>()
+                {
+                    {"filetest", new byte[] { 3, 4, 5, 7} }
+                },
+                FormValues = new Dictionary<string, string>()
+                {
+                    {"formval1", "val1"},
+                    {"formval2", "val2"},
+                },
+                OwnerRoot = "ownerroot",
+                RequestContent = new byte[] { 1, 2, 3, 4, 7 }
+            };
+            var outputStream = new MemoryStream();
+            serData.ToStream(outputStream);
+            var outputData = outputStream.ToArray();
+            var inputStream = new MemoryStream(outputData);
+            var outputSer = inputStream.DeserializeProtobuf<HttpRequestSerializer.HttpRequestPackage>();
+            Assert.AreEqual(serData.OwnerRoot, outputSer.OwnerRoot);
+        }
+
+    }
+}
