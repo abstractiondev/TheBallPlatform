1102da54e6a5369b7b5f7d62c081677dfc773034
Kalle Launiala <kalle.launiala@protonit.net>
2015-07-23 13:21:33 +0300

    Fixed environmental variable usage syntax, added rem to workaround Visual Studio file encoding mark issue

--- Apps/TheBallWebRole/InitializeIIS.ps1
+++ Apps/TheBallWebRole/InitializeIIS.ps1
@@ -1,4 +1,4 @@
 ﻿import-module ServerManager
 Add-WindowsFeature Web-CertProvider
 mkdir D:\TheBallCerts
-Enable-WebCentralCertProvider -CertStoreLocation D:\TheBallCerts -UserName env:$IISCertAccountName -Password env:$IISCertAccountPassword
+Enable-WebCentralCertProvider -CertStoreLocation D:\TheBallCerts -UserName $env:IISCertAccountName -Password $env:IISCertAccountPassword
