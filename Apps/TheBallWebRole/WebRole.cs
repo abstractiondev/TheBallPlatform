a1dd8636c41bb56d3947b978ca8b70fed9cb0466
Kalle Launiala <kalle.launiala@protonit.net>
2015-07-23 20:59:29 +0300

    First "initial sync" concept version for IIS site sync done

--- Apps/TheBallWebRole/WebRole.cs
+++ Apps/TheBallWebRole/WebRole.cs
@@ -1,41 +1,106 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Web.ApplicationServices;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using TheBall.Infra.WebServerManager;
 
 namespace TheBallWebRole
 {
     public class WebRole : RoleEntryPoint
     {
+        private const string SiteContainerName = "tb-sites";
+        //private const string PathTo7Zip = @"d:\bin\7z.exe";
+        private const string PathTo7Zip = @"E:\TheBallInfra\7z\7z.exe";
+
+        private CloudStorageAccount StorageAccount;
+        private CloudBlobClient BlobClient;
+        private CloudBlobContainer SiteContainer;
+
         public override bool OnStart()
         {
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+            var connStr = StorageConnectionString;
 
+            StorageAccount = CloudStorageAccount.Parse(connStr);
+            BlobClient = StorageAccount.CreateCloudBlobClient();
+            SiteContainer = BlobClient.GetContainerReference(SiteContainerName);
 
             SyncWebsitesFromStorage();
             
             return base.OnStart();
         }
 
+        private static string StorageConnectionString => CloudConfigurationManager.GetSetting("StorageConnectionString");
+
+        private static string TempSitesRootFolder
+        {
+            get
+            {
+                var localResource = RoleEnvironment.GetLocalResource("TempSites");
+                return localResource.RootPath;
+            }
+        }
+
+        private static string LiveSitesRootFolder
+        {
+            get
+            {
+                var localResource = RoleEnvironment.GetLocalResource("Sites");
+                return localResource.RootPath;
+            }
+        }
+
         private void SyncWebsitesFromStorage()
         {
-            string websiteFolder;
-            string hostAndSiteName;
-
-            websiteFolder = @"d:\temp\iistest1";
-            hostAndSiteName = "testlocal.theball.me";
-            try
+            var blobs = SiteContainer.ListBlobs(null, true, BlobListingDetails.Metadata);
+            foreach (CloudBlockBlob blob in blobs)
             {
-                IISSupport.CreateCCSWebSite(websiteFolder, hostAndSiteName);
+                string fileName = blob.Name;
+                string hostAndSiteName = Path.GetFileNameWithoutExtension(fileName);
+                string tempFile = Path.Combine(TempSitesRootFolder, blob.Name);
+                FileInfo currentFile = new FileInfo(tempFile);
+                var blobLastModified = blob.Properties.LastModified.GetValueOrDefault().UtcDateTime;
+                if (!currentFile.Exists || currentFile.LastWriteTimeUtc != blobLastModified)
+                {
+                    blob.DownloadToFile(tempFile, FileMode.Create);
+                    currentFile.Refresh();
+                    currentFile.LastWriteTimeUtc = blobLastModified;
+                    UpdateIISSite(tempFile, TempSitesRootFolder, hostAndSiteName, LiveSitesRootFolder);
+                }
+            }
+        }
 
-            }
-            catch (Exception)
+        private void UpdateIISSite(string tempZipFile, string tempSitesRootFolder, string hostAndSiteName, string liveSitesRootFolder)
+        {
+            string tempDirName = Path.Combine(tempSitesRootFolder, hostAndSiteName);
+            var tempDirectory = new DirectoryInfo(tempDirName);
+            if(tempDirectory.Exists)
+                tempDirectory.Delete(true);
+            tempDirectory.Create();
+            var unzipProcInfo = new ProcessStartInfo(PathTo7Zip, String.Format(@"x ..\{0}.zip", hostAndSiteName));
+            unzipProcInfo.WorkingDirectory = tempDirName;
+            var unzipProc = Process.Start(unzipProcInfo);
+            unzipProc.WaitForExit();
+
+            string fullLivePath = Path.Combine(liveSitesRootFolder, hostAndSiteName);
+            IISSupport.UpdateSite(fullLivePath, hostAndSiteName, () =>
             {
-            }
+                string sourceFolder = Path.Combine(tempSitesRootFolder, hostAndSiteName);
+                string targetFolder = fullLivePath;
+                ProcessStartInfo startInfo = new ProcessStartInfo("robocopy.exe", String.Format(@"/MIR ""{0}"" ""{1}""", sourceFolder,
+                    targetFolder));
+                startInfo.UseShellExecute = true;
+                var proc = Process.Start(startInfo);
+                proc.WaitForExit();
+            });
         }
     }
 }
--- Apps/WebServerManager/IISSupport.cs
+++ Apps/WebServerManager/IISSupport.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Web.Administration;
 
@@ -9,19 +10,31 @@ namespace TheBall.Infra.WebServerManager
 {
     public static class IISSupport
     {
-        public static void CreateCCSWebSite(string websiteFolder, string hostAndSiteName)
+        public static Site CreateOrRetrieveCCSWebSite(string websiteFolder, string hostAndSiteName)
         {
             ServerManager iisManager = new ServerManager();
             var sites = iisManager.Sites;
             var existingSite = sites[hostAndSiteName];
             if (existingSite != null)
-                return;
+                return existingSite;
             string bindingInformation = String.Format("*:443:{0}", hostAndSiteName);
             existingSite = sites.Add(hostAndSiteName, "https", bindingInformation, websiteFolder);
             var binding = existingSite.Bindings[0];
             binding.SslFlags = SslFlags.CentralCertStore | SslFlags.Sni;
             iisManager.CommitChanges();
+            return sites[hostAndSiteName];
         }
 
+        public static void UpdateSite(string fullLivePath, string hostAndSiteName, Action action)
+        {
+            var site = CreateOrRetrieveCCSWebSite(fullLivePath, hostAndSiteName);
+            site.Stop();
+            while(site.State != ObjectState.Stopped)
+                Thread.Sleep(200);
+            action();
+            site.Start();
+            while(site.State != ObjectState.Started)
+                Thread.Sleep(200);
+        }
     }
 }
