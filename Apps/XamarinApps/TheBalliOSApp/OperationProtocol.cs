22d676169262a3bc7d2ce6fa2d9fa5628a88d84d
Kalle Launiala <kalle.launiala@protonit.net>
2015-10-01 19:41:59 +0300

    First version of custom .json files serving iOS webview app functional

--- Apps/XamarinApps/TheBalliOSApp/OperationProtocol.cs
+++ Apps/XamarinApps/TheBalliOSApp/OperationProtocol.cs
@@ -1,4 +1,6 @@
-﻿using Foundation;
+﻿using System;
+using System.IO;
+using Foundation;
 
 namespace TheBalliOSApp
 {
@@ -7,10 +9,16 @@ namespace TheBalliOSApp
         [Export("canInitWithRequest:")]
         public static bool canInitWithRequest(NSUrlRequest request)
         {
-            if (request.Url.Path != null)
+            var urlPath = request.Url.Path;
+            if (urlPath != null)
             {
-                bool isOperation = request.Url.Path.Contains("/op/");
-                return isOperation;
+                bool isOperation = urlPath.Contains("/op/");
+                if(isOperation)
+                    return true;
+                var absolutePath = request.Url.AbsoluteString;
+                bool isData = absolutePath.StartsWith(DataPrefix);
+                if (isData)
+                    return true;
             }
             return false;
         }
@@ -27,9 +35,56 @@ namespace TheBalliOSApp
         {
         }
 
+        private const string DataPrefix = "file:///data/";
+        private readonly DataRetriever CustomDataRetriever = key => null;
+        public delegate Tuple<string, string, Stream> DataRetriever(string dataKey);
+
+
         public override void StartLoading()
         {
             var request = Request;
+            var url = request.Url.AbsoluteString;
+            if (url.StartsWith(DataPrefix))
+            {
+                string dataKey = url.Substring(DataPrefix.Length);
+                var customData = CustomDataRetriever(dataKey);
+                if (customData != null)
+                {
+                    using (var response = new NSUrlResponse(request.Url, customData.Item1, -1, customData.Item2))
+                    {
+                        Client.ReceivedResponse(this, response, NSUrlCacheStoragePolicy.NotAllowed);
+                        this.InvokeOnMainThread(() =>
+                        {
+                            using (var data = NSData.FromStream(customData.Item3))
+                            {
+                                Client.DataLoaded(this, data);
+                            }
+                            Client.FinishedLoading(this);
+                        });
+                    }
+                    return;
+                }
+                var fixedUrl = url.Replace(DataPrefix, "Content/CoreUI/data/");
+                var fileName = Path.Combine(NSBundle.MainBundle.BundlePath, fixedUrl);
+                if (!File.Exists(fileName))
+                    return;
+                using (var response = new NSUrlResponse(request.Url, "application/json", -1, "utf-8"))
+                {
+                    Client.ReceivedResponse(this, response, NSUrlCacheStoragePolicy.NotAllowed);
+                    this.InvokeOnMainThread(() =>
+                    {
+                        using (var sourceStream = File.OpenRead(fileName))
+                        {
+                            using (var data = NSData.FromStream(sourceStream))
+                                Client.DataLoaded(this, data);
+                            Client.FinishedLoading(this);
+                        }
+
+                    });
+                }
+                return;
+            }
+
             var client = Client;
             /*
             var value = Request.Url.Path.Substring(1);
