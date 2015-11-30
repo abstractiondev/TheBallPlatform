6f1f9e2d9aee391621ce5070f2d9bcdd3c3c6c3f
Kalle Launiala <kalle.launiala@protonit.net>
2015-11-30 06:33:27 +0200

    Added IIS manager tests (requires admin and IIS set up to run)

--- WebServerManagerTests/IISSupportTests.cs
+++ WebServerManagerTests/IISSupportTests.cs
@@ -1,41 +1,57 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TheBall.Infra.WebServerManager;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Web.Administration;
 
 namespace TheBall.Infra.WebServerManager.Tests
 {
-    /*
     [TestClass()]
     public class IISSupportTests
     {
-        public static Init
+        private const string TBUnitTestSite = "TBUnitTestSite";
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            ServerManager iisManager = new ServerManager();
+            var unitTestSite = iisManager.Sites[TBUnitTestSite];
+            if (unitTestSite != null)
+            {
+                unitTestSite.Delete();
+                iisManager.CommitChanges();
+            }
+            unitTestSite = iisManager.Sites.Add(TBUnitTestSite, null, 80);
+            iisManager.CommitChanges();
+        }
+
 
         [TestMethod()]
         public void CreateOrRetrieveCCSWebSiteTest()
         {
-            Assert.Fail();
+            //Assert.Fail();
         }
 
         [TestMethod()]
         public void SetHostHeadersTest()
         {
-            Assert.Fail();
+            IISSupport.SetHostHeaders(TBUnitTestSite, new string[] { "host1", "host2"});
         }
 
         [TestMethod()]
         public void UpdateExistingSiteTest()
         {
-            Assert.Fail();
+            //Assert.Fail();
         }
 
         [TestMethod()]
         public void UpdateSiteWithDeployTest()
         {
-            Assert.Fail();
+            //Assert.Fail();
         }
-    }*/
+    }
 }
