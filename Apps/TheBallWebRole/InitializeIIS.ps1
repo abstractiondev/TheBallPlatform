69628dfdb990a115e284bc9f98961f598959c9e1
Kalle Launiala <kalle.launiala@protonit.net>
2015-12-02 19:43:27 +0200

    Added InitializeIIS to include ApplicationInit

--- Apps/TheBallWebRole/InitializeIIS.ps1
+++ Apps/TheBallWebRole/InitializeIIS.ps1
@@ -1,5 +1,6 @@
 ﻿import-module ServerManager
 Add-WindowsFeature Web-CertProvider
+pkgmgr.exe /iu:IIS-ApplicationInit
 mkdir D:\TheBallCerts
 robocopy.exe /MIR X:\TheBallCerts D:\TheBallCerts
 Enable-WebCentralCertProvider -CertStoreLocation D:\TheBallCerts -UserName $env:IISCertAccountName -Password $env:IISCertAccountPassword -PrivateKeyPassword T11r1kka
