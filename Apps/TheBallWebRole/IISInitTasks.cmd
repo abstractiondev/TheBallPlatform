f52efe6621b7451b2bfc51a1437fff0f425278bf
Kalle Launiala <kalle.launiala@protonit.net>
2015-12-21 19:02:27 +0200

    Added Powershell scripts to import Pfx-certs to machine store (to be applied properly to sites)

--- Apps/TheBallWebRole/IISInitTasks.cmd
+++ Apps/TheBallWebRole/IISInitTasks.cmd
@@ -1,3 +1,4 @@
 ﻿REM Use Powershell to run IIS configs
 PowerShell -Version 4.0 -Command "Set-Executionpolicy Unrestricted"
 PowerShell -Version 4.0 .\InitializeIIS.ps1
+PowerShell -Version 4.0 .\InstallCerts.ps1
---
+++ Apps/TheBallWebRole/InstallCerts.ps1
@@ -0,0 +1,7 @@
+﻿Get-ChildItem "D:\TheBallCerts" -Filter *.pfx | `
+Foreach-Object{
+    $certFileName = $_.FullName
+    Write-Host $certFileName
+    $myPwd = ConvertTo-SecureString -String "T11r1kka" -Force -AsPlainText
+    Import-PfxCertificate -FilePath $certFileName -Password $myPwd -CertStoreLocation Cert:\LocalMachine\My
+}
