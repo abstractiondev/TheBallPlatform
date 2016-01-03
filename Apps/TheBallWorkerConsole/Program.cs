3d1708b9c8963b07d79203152b59644458a4e3c4
Kalle Launiala <kalle.launiala@protonit.net>
2016-01-03 18:51:25 +0200

    Added ensure X: drive mount to worker

--- Apps/TheBallWorkerConsole/Program.cs
+++ Apps/TheBallWorkerConsole/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.Azure;
 using Nito.AsyncEx;
 
-namespace TheBall.CORE.TheBallWorkerConsole
+namespace TheBall.Infra.TheBallWorkerConsole
 {
     class Program
     {
@@ -37,6 +40,7 @@ namespace TheBall.CORE.TheBallWorkerConsole
 
         static async void MainAsync(string[] args)
         {
+            ensureXDrive();
             var configFullPath = args.Length > 0 ? args[0] : null;
             if (configFullPath == null)
                 throw new ArgumentNullException(nameof(args), "Config full path cannot be null (first  argument)");
@@ -55,8 +59,10 @@ namespace TheBall.CORE.TheBallWorkerConsole
                 Task[] activeTasks = new Task[ConcurrentTasks];
                 int nextFreeIX = 0;
 
+                var instanceNames = workerConfig.InstancePollItems.Select(item => item.InstanceHostName).ToArray();
+
                 string startupLogPath = Path.Combine(AssemblyDirectory, "ConsoleStartupLog.txt");
-                var startupMessage = "Starting up process (UTC): " + DateTime.UtcNow.ToString();
+                var startupMessage = "Starting up process (UTC): " + DateTime.UtcNow.ToString() + " for instances: " + String.Join(", ", instanceNames);
                 File.WriteAllText(startupLogPath, startupMessage);
 
                 var pipeMessageAwaitable = reader != null ? reader.ReadToEndAsync() : null;
@@ -86,6 +92,26 @@ namespace TheBall.CORE.TheBallWorkerConsole
                 {
                     reader.Dispose();
                     pipeStream.Dispose();
+                }
+            }
+        }
+
+        private static void ensureXDrive()
+        {
+            bool hasDriveX = DriveInfo.GetDrives().Any(item => item.Name.ToLower().StartsWith("X"));
+            if (!hasDriveX)
+            {
+                var infraAccountName = CloudConfigurationManager.GetSetting("CoreFileShareAccountName");
+                var infraAccountKey = CloudConfigurationManager.GetSetting("CoreFileShareAccountKey");
+                bool isCloud = infraAccountName != null && infraAccountKey != null;
+                if (isCloud)
+                {
+                    var netPath = Path.Combine(Environment.SystemDirectory, "net.exe");
+                    var args = $@"use X: \\{infraAccountName}.file.core.windows.net\tbcore /u:{infraAccountName} {infraAccountKey}";
+                    var startInfo = new ProcessStartInfo(netPath) {UseShellExecute = false, Arguments = args};
+                    var netProc = new Process {StartInfo = startInfo};
+                    netProc.Start();
+                    netProc.WaitForExit();
                 }
             }
         }
--- Apps/TheBallWorkerConsole/WorkerConsoleConfig.cs
+++ Apps/TheBallWorkerConsole/WorkerConsoleConfig.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using AzureSupport;
 
-namespace TheBall.CORE.TheBallWorkerConsole
+namespace TheBall.Infra.TheBallWorkerConsole
 {
     public class WorkerConsoleConfig
     {
