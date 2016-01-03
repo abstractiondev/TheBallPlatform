7c7210e49fab67475d56218cc37928fe4650eac7
Kalle Launiala <kalle.launiala@protonit.net>
2016-01-03 17:51:01 +0200

    Consolidated and updated JSON package to be Json.NET also on WorkerConsole

--- Apps/TheBallWorkerConsole/Program.cs
+++ Apps/TheBallWorkerConsole/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using Nito.AsyncEx;
 
-namespace TheBallWorkerConsole
+namespace TheBall.CORE.TheBallWorkerConsole
 {
     class Program
     {
