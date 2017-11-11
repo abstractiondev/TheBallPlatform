b3bf1130e865fcf2a93ae05b0b9cb41430edc7d4
Kalle Launiala <kalle.launiala@protonit.net>
2017-11-11 21:15:11 +0200

    Fixed ghost character at publish_release.cmd

--- Apps/TheBallWorkerConsole/publish_release.cmd
+++ Apps/TheBallWorkerConsole/publish_release.cmd
@@ -1,2 +1,2 @@
-﻿"c:\program files\dotnet\dotnet" publish -c release -r win-x64 -f netcoreapp2.0
-"c:\program files\dotnet\dotnet" publish -c release -r linux-x64 -f netcoreapp2.0
+"c:\program files\dotnet\dotnet.exe" publish -c release -r win-x64 -f netcoreapp2.0
+"c:\program files\dotnet\dotnet.exe" publish -c release -r linux-x64 -f netcoreapp2.0
