339ca9c219745a5c1819814aa612963797763af5
Kalle Launiala <kalle.launiala@protonit.net>
2018-06-04 20:42:03 +0300

    Updated properly working https parameters

--- Apps/WebCoreLayer/Program.cs
+++ Apps/WebCoreLayer/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -25,9 +28,10 @@ namespace WebCoreLayer
             var certPassword =
                 Environment.GetEnvironmentVariable("TBCertificatePassword");
             var certPath = @"X:\TheBallCerts\wildcard_theball.me.pfx";
-            if (!File.Exists(certPath))
+            if (!File.Exists(certPath) && Startup.PlatformCoreRootPath != null)
                 certPath = Path.Combine(Startup.PlatformCoreRootPath, "TheBallCerts", "wildcard_theball.me.pfx");
             bool useHttps = !String.IsNullOrEmpty(certPassword) && File.Exists(certPath);
+            useHttps = false;
             var port = useHttps ? 10443 : 80;
 
             var result = WebHost.CreateDefaultBuilder(args)
@@ -48,7 +52,16 @@ namespace WebCoreLayer
                     options.Listen(IPAddress.Any, port, listenOptions =>
                     {
                         if (useHttps)
-                            listenOptions.UseHttps(certPath, certPassword);
+                        {
+                            //listenOptions.UseHttps(certPath, certPassword);
+                            listenOptions.UseHttps(new HttpsConnectionAdapterOptions()
+                            {
+                                ClientCertificateMode = ClientCertificateMode.NoCertificate,
+                                ServerCertificate = new X509Certificate2(certPath, certPassword),
+                                SslProtocols = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12,
+                                CheckCertificateRevocation = false
+                            });
+                        }
                     });
                 })
                 .Build();
