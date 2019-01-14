a425b6c4ba46865e6b3d7f2aac1a5fbc6d03ce3d
Kalle Launiala <kalle.launiala@protonit.net>
2019-01-14 20:39:21 +0200

    Added API definition documents as resources to be obtainable through API

--- Tests/PlatformCoreTests/ServerAPITests.cs
+++ Tests/PlatformCoreTests/ServerAPITests.cs
@@ -1,5 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebCoreLayer.Controllers;
 
@@ -8,6 +14,55 @@ namespace PlatformCoreTests
     [TestClass]
     public class ServerAPITests
     {
+        [TestMethod]
+        public void VerifyAllResourceNames()
+        {
+            /*
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(asm => !asm.IsDynamic)
+                .Where(asm => asm.FullName.Contains("AzureSupport"))
+                //.Concat(new Assembly[] { typeof(AzureSupport.WebSupport).Assembly})
+                .ToArray();*/
+            var assemblies = new Assembly[] { typeof(AzureSupport.WebSupport).Assembly};
+            Parallel.ForEach(assemblies, asm =>
+            {
+                var names = asm.GetManifestResourceNames().Where(name => name.ToLower().EndsWith(".xml"))
+                    .ToArray();
+                Parallel.ForEach(names, name =>
+                {
+                    var resourceStream = asm.GetManifestResourceStream(name);
+                    XDocument xDoc = null;
+                    using (var reader = XmlReader.Create(resourceStream))
+                    {
+                        try
+                        {
+                            xDoc = XDocument.Load(reader);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                    if (xDoc == null)
+                        return;
+                    var theBallElement = xDoc.Descendants()
+                        .FirstOrDefault(elem => elem.Name.LocalName == "InstanceOfTheBall");
+                    theBallElement = theBallElement ?? xDoc.Elements("InstanceOfTheBall").FirstOrDefault();
+                    var semanticName = theBallElement?.Attribute("semanticDomainName")?.Value;
+                    if (semanticName == null)
+                        return;
+                    var nameSplit = name.Split('.');
+                    var namespaceLength = nameSplit.Length - 3;
+                    var assemblyLength = 1;
+                    var namespacePart = String.Join('.', nameSplit.Skip(assemblyLength).Take(namespaceLength).ToArray());
+                    var contentNamePart = String.Join('.', nameSplit.Skip(assemblyLength).Skip(namespaceLength).ToArray());
+                    var expectedName = namespacePart.Replace(".", "") + ".xml";
+                    Assert.AreEqual(expectedName, contentNamePart);
+                    Console.WriteLine($"Validated resource: {namespacePart}.{contentNamePart}");
+                });
+            });
+        }
+
         [TestMethod]
         public async Task GetAPIXmlFromControllerTest()
         {
