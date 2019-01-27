00b4283eacbbedc70fedb53b6d9c8495c3e51776
Kalle Launiala <kalle.launiala@protonit.net>
2019-01-27 15:35:39 +0200

    Working Kubernetes concept test; gets pods and containers OK

--- Tools/KubeTool/Program.cs
+++ Tools/KubeTool/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using k8s;
 
 namespace KubeTool
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(config);
+            var podResult = await client.ListPodForAllNamespacesWithHttpMessagesAsync();
+            var containers = podResult.Body.Items.SelectMany(item => item.Spec.Containers).ToArray();
+            var containerNames = containers.Select(item => item.Image).OrderBy(item => item).ToArray();
         }
     }
 }
