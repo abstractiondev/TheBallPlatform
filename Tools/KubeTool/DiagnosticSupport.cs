612a7aed7b3eb5efa5fde67c03d3b4165916aab4
Kalle Launiala <kalle.launiala@protonit.net>
2019-01-30 08:41:29 +0200

    Kubernetes deployment image update - to latest or specific concept test implemented

--- Tools/KubeTool/DiagnosticSupport.cs
+++ Tools/KubeTool/DiagnosticSupport.cs
@@ -7,11 +7,29 @@ namespace KubeTool
     {
         public static async Task RunBaseDiagnostics()
         {
-            string workerImageName = @"abstractiondev/theballworker";
-            var latestWorkerTag = await DockerHubSupport.GetLatestTag(workerImageName);
-            var workerDeployment = await KubeSupport.GetDeployment("tbwrk-deployment");
-            var containers = workerDeployment?.Spec.Template.Spec.Containers;
-            var isRunningLatest = containers.Any(item => item.Image.StartsWith($"{workerImageName}:{latestWorkerTag}"));
+            return;
+            string workerBaseName = @"abstractiondev/theballworker";
+            var workerDeploymentName = "tbwrk-deployment";
+            await UpdateDeployment(workerBaseName, workerDeploymentName);
+
+            string webBaseName = @"abstractiondev/theballweb";
+            var webDeploymentName = "tbweb-deployment";
+            //await UpdateDeployment(webBaseName, webDeploymentName, "20180802.1631_dev_126cbdbf4dc44315dc1578d15ef7a7726a7e26c9");
+            await UpdateDeployment(webBaseName, webDeploymentName);
+        }
+
+        private static async Task UpdateDeployment(string imageBaseName, string deploymentName, string specificTag = null)
+        {
+            var latestImageTag = await DockerHubSupport.GetLatestTag(imageBaseName);
+            var updateTag = specificTag ?? latestImageTag;
+            var deployment = await KubeSupport.GetDeployment(deploymentName);
+            var containers = deployment?.Spec.Template.Spec.Containers;
+            var imageName = $"{imageBaseName}:{updateTag}";
+            var isRunningExpected = containers.Any(item => item.Image.StartsWith(imageName));
+            if (!isRunningExpected)
+            {
+                await KubeSupport.UpdateDeployment(deploymentName, imageName);
+            }
         }
     }
 }
--- Tools/KubeTool/KubeSupport.cs
+++ Tools/KubeTool/KubeSupport.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using k8s;
 using k8s.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.Rest.Serialization;
 
 namespace KubeTool
 {
@@ -39,6 +43,25 @@ namespace KubeTool
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
             var client = new Kubernetes(config);
             return client;
+        }
+
+        public static async Task UpdateDeployment(string deploymentName, string imageName)
+        {
+            var client = GetClient();
+            var deployment = await GetDeployment(deploymentName);
+            var currentImageName = deployment.Spec.Template.Spec.Containers.First().Image;
+            var currentImageBaseName = currentImageName.Split(':').First();
+            var updateImageBaseName = imageName.Split(':').First();
+            if(currentImageBaseName != updateImageBaseName)
+                throw new InvalidOperationException($"Not allowed to update image {currentImageName} with incompatible base {imageName}");
+            var namespaceName = deployment.Metadata.NamespaceProperty;
+            //deployment.Spec.Template.Spec.Containers.FirstOrDefault()
+            IJsonPatchDocument jsonPatch = new JsonPatchDocument(new List<Operation>()
+            {
+                new Operation("replace", "/spec/template/spec/containers/0/image", null, imageName)
+            }, new ReadOnlyJsonContractResolver());
+            V1Patch patchBody = new V1Patch(jsonPatch);
+            await client.PatchNamespacedDeploymentWithHttpMessagesAsync(patchBody, deploymentName, namespaceName);
         }
     }
 }
